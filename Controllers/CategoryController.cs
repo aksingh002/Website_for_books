6067fee894c33b6a27603e82c274f3a74be5dd77
aksingh002 <aksinghcool007@gmail.com>
2023-07-29 23:01:25 +0530

    after category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -27,6 +27,76 @@ namespace dotNet_webApplication.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(Category obj){
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name","the display order is same is Name");
+            }
+            if(ModelState.IsValid){
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id){
+            if(id==null || id==0){
+                return NotFound();
+
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj){
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name","the display order is same is Name");
+            }
+            if(ModelState.IsValid){
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id){
+            if(id==null || id==0){
+                return NotFound();
+
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id){
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+           
+        }
+
         
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +11,11 @@ namespace dotNet_webApplication.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public required string Name { get; set; }
+        [DisplayName("Display order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
 
     }
