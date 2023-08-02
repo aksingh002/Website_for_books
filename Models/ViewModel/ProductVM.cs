4dfb6613a6c7aa8a6e8ec0c06533753d50bff9f8
aksingh002 <aksinghcool007@gmail.com>
2023-08-02 19:01:56 +0530

    new Commit

---
+++ Models/ViewModel/ProductVM.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet_webApplication.Models.ViewModel
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        public Category Category { get; set; }
+    }
+}
